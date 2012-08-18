2702a384342cf5156498e9b04dd58f5c3535658f
Hilverd Reker <me@hilverd.com>
2012-08-19 00:14:54 +0100

    Make Puppet configuration idempotent.

--- modules/oracle/manifests/init.pp
+++ modules/oracle/manifests/init.pp
@@ -1,40 +1,52 @@
 class oracle::server {
   exec {
     "/usr/bin/apt-get -y update":
-      alias => "aptUpdate",
+      alias => "apt-update",
       timeout => 3600;
   }
 
   package {
     "alien":
+      require => Exec["apt-update"],
       ensure => installed;
     "bc":
+      require => Exec["apt-update"],
       ensure => installed;
     "curl":
+      require => Exec["apt-update"],
       ensure => installed;
     "git":
+      require => Exec["apt-update"],
       ensure => installed;
     "htop":
+      require => Exec["apt-update"],
       ensure => installed;
     "libaio1":
+      require => Exec["apt-update"],
       ensure => installed;
     "monit":
+      require => Exec["apt-update"],
       ensure => installed;
     "ntp":
+      require => Exec["apt-update"],
       ensure => installed;
     "rsyslog":
+      require => Exec["apt-update"],
       ensure => installed;
     "unixodbc":
+      require => Exec["apt-update"],
       ensure => installed;
     "unzip":
+      require => Exec["apt-update"],
       ensure => installed;
   }
 
   service {
+    "monit":
+      require => Package["monit"],
+      ensure => running;
     "ntp":
       ensure => stopped;
-    "monit":
-      ensure => running;
     "rsyslog":
       ensure => running;
     "procps":
@@ -67,7 +79,8 @@ class oracle::server {
     "set up shm":
       command => "/etc/rc2.d/S01shm_load start",
       require => File["/etc/rc2.d/S01shm_load"],
-      user => root;
+      user => root,
+      unless => "/bin/mount | grep /dev/shm 2>/dev/null";
   }
 }
 
@@ -80,20 +93,23 @@ class oracle::swap {
     "set up swapfile":
       command => "/sbin/mkswap /swapfile",
       require => Exec["create swapfile"],
-      user => root;
+      user => root,
+      unless => "/usr/bin/file /swapfile | grep 'swap file' 2>/dev/null";
     "enable swapfile":
       command => "/sbin/swapon /swapfile",
       require => Exec["set up swapfile"],
-      user => root;
+      user => root,
+      unless => "/bin/cat /proc/swaps | grep '^/swapfile' 2>/dev/null";
     "add swapfile entry to fstab":
       command => "/bin/echo >>/etc/fstab /swapfile swap swap defaults 0 0",
-      user => root;
+      user => root,
+      unless => "/bin/grep '^/swapfile' /etc/fstab 2>/dev/null";
   }
 }
 
 class oracle::xe {
   file {
-    "/tmp/oracle-xe-11.2.0-1.0.x86_64.rpm.zip":
+    "/home/vagrant/oracle-xe-11.2.0-1.0.x86_64.rpm.zip":
       source => "puppet:///modules/oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip";
     "/etc/profile.d/oracle-env.sh":
       source => "puppet:///modules/oracle/oracle-env.sh";
@@ -110,17 +126,19 @@ class oracle::xe {
 
   exec {
     "unzip xe":
-      command => "/usr/bin/unzip -o /tmp/oracle-xe-11.2.0-1.0.x86_64.rpm.zip",
-      require => [Package["unzip"], File["/tmp/oracle-xe-11.2.0-1.0.x86_64.rpm.zip"]],
-      cwd => "/tmp",
+      command => "/usr/bin/unzip -o oracle-xe-11.2.0-1.0.x86_64.rpm.zip",
+      require => [Package["unzip"], File["/home/vagrant/oracle-xe-11.2.0-1.0.x86_64.rpm.zip"]],
+      cwd => "/home/vagrant",
       user => root,
-      creates => "/tmp/oracle-xe-11.2.0-1.0.x86_64.rpm";
+      creates => "/home/vagrant/oracle-xe-11.2.0-1.0.x86_64.rpm",
+      unless => "/usr/bin/test -f /etc/default/oracle-xe";
     "alien xe":
-      command => "/usr/bin/alien --to-deb --scripts /tmp/Disk1/oracle-xe-11.2.0-1.0.x86_64.rpm",
-      cwd => "/tmp/Disk1",
+      command => "/usr/bin/alien --to-deb --scripts Disk1/oracle-xe-11.2.0-1.0.x86_64.rpm",
+      cwd => "/home/vagrant",
       require => [Package["alien"], Exec["unzip xe"]],
-      creates => "/tmp/Disk1/oracle-xe_11.2.0-2_amd64.deb",
-      user => root;
+      creates => "/home/vagrant/oracle-xe_11.2.0-2_amd64.deb",
+      user => root,
+      unless => "/usr/bin/test -f /etc/default/oracle-xe";
     "configure xe":
       command => "/etc/init.d/oracle-xe configure responseFile=/tmp/xe.rsp >> /tmp/xe-install.log",
       require => [Package["oracle-xe"],
@@ -128,7 +146,8 @@ class oracle::xe {
                   File["/tmp/xe.rsp"],
                   File["/var/lock/subsys/listener"],
                   Exec["set up shm"],
-                  Exec["enable swapfile"]];
+                  Exec["enable swapfile"]],
+      creates => "/etc/default/oracle-xe";
   }
   
   package {
@@ -136,6 +155,6 @@ class oracle::xe {
       provider => "dpkg",
       ensure => latest,
       require => [Exec["alien xe"]],
-      source => "/tmp/Disk1/oracle-xe_11.2.0-2_amd64.deb";
+      source => "/home/vagrant/oracle-xe_11.2.0-2_amd64.deb",
   }
 }
